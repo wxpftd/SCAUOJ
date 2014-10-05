c8209143018fcdbf8651623b8c71895bac32eff8
ftd <zxc2012@gmail.com>
2014-10-06 00:59:02 +0800

    One shoot

--- 11085.cpp
+++ 11085.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+
+using namespace std;
+
+long long fun(long long n, long long tempNums[])
+{
+    if (-1 != tempNums[n])
+    {
+        return tempNums[n];      
+    }
+    long long result = 0;
+    for (int i=0; i<n; i+=2)
+    {
+        result = result + fun(i, tempNums) * fun(n-2-i, tempNums);
+    }
+    tempNums[n] = result;
+    return result;
+}
+
+int main()
+{
+    long long tempNums[50];
+    tempNums[0] = 1;
+    for (int i = 1; i < 50; i++)
+    {
+        tempNums[i] = -1; 
+    }
+    long long n = 0;
+    cin >> n;
+    cout << fun(2*n, tempNums) << endl;
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+a.out: 11085.cpp
+	g++ -Wall -std=c++11 -o a.out 11085.cpp
+
+clean:
+	rm a.out
